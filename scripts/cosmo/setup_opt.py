d13120da363bb699487185af8d8e2264fa48319a
rubind <davidrubinlbl@gmail.com>
2017-04-20 09:03:37 -0700

    setup

--- scripts/cosmo/setup_opt.py
+++ scripts/cosmo/setup_opt.py
@@ -1,9 +1,16 @@
 import commands
+import sys
 
+hostname = commands.getoutput("hostname")
 
 
 binw = "'" + '"Binw"' + "'"
 detf = "'" + '"DETF"' +"'"
+
+together = int(sys.argv[1])
+
+if together:
+    f = open("tmp.sh", 'w')
 
 for slew_time in [0, 70]:
     for run in range(3):
@@ -16,8 +23,19 @@ for slew_time in [0, 70]:
             commands.getoutput("mkdir " + wd)
             commands.getoutput("cp paramfile_wrap.txt " + wd)
 
-            f = open("tmp.sh", 'w')
-            f.write("""#!/bin/bash -l
+            if not together:
+                f = open("tmp.sh", 'w')
+
+            if hostname.count("rivoli"):
+                f.write("cd " + commands.getoutput("pwd") + "/" + wd + '\n')
+                f.write("unbuffer ~/anaconda2/bin/python ../optimize_survey.py " + "%.2f %s > log.txt & \n" % (slew_time, FoM_type))
+                
+                if not together:
+                    f.close()
+                    print commands.getoutput("qsub tmp.sh")
+
+            else:
+                f.write("""#!/bin/bash -l
 #SBATCH --partition=shared
 #SBATCH --nodes=1
 #SBATCH --time=48:00:00
@@ -26,7 +44,9 @@ for slew_time in [0, 70]:
 module load python/2.7-anaconda
 cd """ + commands.getoutput("pwd") + "/" + wd + """
 srun -n 1 -c 1 python ../optimize_survey.py """ + "%.2f %s > log.txt & \n" % (slew_time, FoM_type))
-            f.close()
+                f.close()
 
-            print commands.getoutput("sbatch tmp.sh")
+                print commands.getoutput("sbatch tmp.sh")
 
+if together:
+    f.close()
