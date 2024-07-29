cb877ff06fd62d7b1acdd4a3c9b508f636741bb2
David Rubin <drubin@cn-03-13-02.mana.ci.its.hawaii.edu>
2024-07-29 04:03:43 +0000

    redo

---
+++ scripts/stan_cosmo/STEP3_redo_fisher.py
@@ -0,0 +1,29 @@
+import glob
+from subprocess import getoutput
+
+pwd = getoutput("pwd")
+
+for dr in glob.glob("*"):
+    f = open("tmp.sh", 'w')
+    f.write("""#!/bin/bash
+#SBATCH --job-name=sim
+#SBATCH --partition=shared
+#SBATCH --time=0-08:00:00 ## time format is DD-HH:MM:SS
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G # Memory per node my job requires
+#SBATCH --error=example-%A.err # %A - filled with jobid, where to write the stderr
+#SBATCH --output=example-%A.out # %A - filled with jobid, wher to write the stdout
+source ~/.bash_profile
+export WFIRST=/home/drubin/wfirst-sim/
+export WFIRST_SIM_DATA=/home/drubin/wfirst-sim-data/
+pip install sncosmo
+pip install sep
+
+cd %s/%s
+python $WFIRST/scripts/stan_cosmo/STEP2_Analytic_Fisher.py survey.pickle --SNRMax 0 --model_res 9 --gray_disp 0.08 > fisher_log.txt
+python $WFIRST/scripts/stan_cosmo/FoM.py comb_mat*fits""" % (pwd, dr))
+    f.close()
+
+    print(getoutput("batch tmp.sh"))
+    
