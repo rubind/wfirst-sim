b1efefb5529b994a9da6622370831663a81fb2e2
David Rubin <David>
2024-10-20 23:07:35 -1000

    grid ready

--- scripts/stan_cosmo/STEP1_survey_grid.py
+++ scripts/stan_cosmo/STEP1_survey_grid.py
@@ -440,7 +440,7 @@ percent_prism10000 = []
 for i in range(10000):
     this_percent = 10000
     while this_percent >= 99.9:
-        this_percent = np.random.exponential()*30
+        this_percent = np.random.random()*20 + 10
     percent_prism10000.append(this_percent)
     
 #percent_prism10000 = np.arange(15, 26, 1)
@@ -457,7 +457,9 @@ grid_vals = dict(widepercent_imaging = np.arange(0, 101, 5),
                  med_filts = ["RZYJHF"],# "RZYJHF", "ZYJHF", "YJHF", "RZYJH", "RZYJ", "ZYJH"],
                  deep_filts = ["RRZYJH", "RZZYJH", "ZZRYJHF", "ZZYJHF", "YYJHF"],
                  wide_cadence = [8, 10],
-                 wide_ztarg = np.arange(0.3, 1.1, 0.1), med_ztarg = np.arange(0.8, 1.3, 0.1), deep_ztarg = np.arange(1.0, 2.5, 0.1),
+                 wide_ztarg = np.arange(0.3, 1.1, 0.1), med_ztarg = [1.], #np.arange(0.8, 1.3, 0.1),
+                 deep_ztarg = np.arange(1.0, 2.5, 0.1),
+                 
                  med_cadence = [10], #[4, 5, 6, 7, 8, 9, 10], #, 12, 15],
                  deep_cadence = [8, 10], #[4, 5, 6, 7, 8, 9, 10, 12, 15],
                  widepercent_prism = np.arange(0, 41, 2), #[0],#np.arange(0, 41, 2),
