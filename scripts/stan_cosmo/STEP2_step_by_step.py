60f1aa44e002c3366ee58066bd5c3ff2301f9a3c
David Rubin <drubin@cn-03-13-02.mana.ci.its.hawaii.edu>
2025-02-13 16:04:59 +0000

    STEP2_step_by_step.py

--- scripts/stan_cosmo/STEP2_step_by_step.py
+++ scripts/stan_cosmo/STEP2_step_by_step.py
@@ -11,7 +11,7 @@ def run_job(train, zpunc, slopeunc, crnlunc, waveunc, wd):
 #SBATCH --time=0-05:00:00 ## time format is DD-HH:MM:SS
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G # Memory per node my job requires
+#SBATCH --mem=40G # Memory per node my job requires
 #SBATCH --error=example-%A.err # %A - filled with jobid, where to write the stderr
 #SBATCH --output=example-%A.out # %A - filled with jobid, wher to write the stdout
 
